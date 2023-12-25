8539096a5c99504e81558cf48d920c744fd7c8b7
Myllena Cerqueira <myllenacerq.contato@gmail.com>
2023-12-25 19:06:00 -0300

    criacao da sessao services e atualizacao da estilizacao do css

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
     $(this).toggleClass('fa-times')
     $('.navbar').toggleClass('nav-toggle')
   })
+
+  $(window).on('load scroll', function(){
+    $('#menu').removeClass('fa-times')
+    $('.navbar').removeClass('nav-toggle')
+  })
+
 })
